8b5a6aad7b7c59232e60bd5c24a7f48a880a8489
Dev-R🌹 <rabimhd@icloud.com>
2022-06-07 01:26:54 +0800

    Added primevue and bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,157 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <Menubar :model="items" />
+    <router-view />
 </template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Menubar from "primevue/menubar";
+export default {
+    name: "HomeView",
+    data() {
+        return {
+            items: [
+                {
+                    label: "Active Listings",
+                    icon: "pi pi-fw pi-file",
+                    items: [
+                        {
+                            label: "New",
+                            icon: "pi pi-fw pi-plus",
+                            items: [
+                                {
+                                    label: "Bookmark",
+                                    icon: "pi pi-fw pi-bookmark"
+                                },
+                                {
+                                    label: "Video",
+                                    icon: "pi pi-fw pi-video"
+                                }
+                            ]
+                        },
+                        {
+                            label: "Delete",
+                            icon: "pi pi-fw pi-trash"
+                        },
+                        {
+                            separator: true
+                        },
+                        {
+                            label: "Export",
+                            icon: "pi pi-fw pi-external-link"
+                        }
+                    ]
+                },
+                {
+                    label: "Categories",
+                    icon: "pi pi-fw pi-pencil",
+                    items: [
+                        {
+                            label: "Left",
+                            icon: "pi pi-fw pi-align-left"
+                        },
+                        {
+                            label: "Right",
+                            icon: "pi pi-fw pi-align-right"
+                        },
+                        {
+                            label: "Center",
+                            icon: "pi pi-fw pi-align-center"
+                        },
+                        {
+                            label: "Justify",
+                            icon: "pi pi-fw pi-align-justify"
+                        }
+                    ]
+                },
+                {
+                    label: "Watchlist",
+                    icon: "pi pi-fw pi-user",
+                    items: [
+                        {
+                            label: "New",
+                            icon: "pi pi-fw pi-user-plus"
+                        },
+                        {
+                            label: "Delete",
+                            icon: "pi pi-fw pi-user-minus"
+                        },
+                        {
+                            label: "Search",
+                            icon: "pi pi-fw pi-users",
+                            items: [
+                                {
+                                    label: "Filter",
+                                    icon: "pi pi-fw pi-filter",
+                                    items: [
+                                        {
+                                            label: "Print",
+                                            icon: "pi pi-fw pi-print"
+                                        }
+                                    ]
+                                },
+                                {
+                                    icon: "pi pi-fw pi-bars",
+                                    label: "List"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: "Create Listing",
+                    icon: "pi pi-fw pi-calendar",
+                    items: [
+                        {
+                            label: "Edit",
+                            icon: "pi pi-fw pi-pencil",
+                            items: [
+                                {
+                                    label: "Save",
+                                    icon: "pi pi-fw pi-calendar-plus"
+                                },
+                                {
+                                    label: "Delete",
+                                    icon: "pi pi-fw pi-calendar-minus"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    label: "Logut",
+                    icon: "pi pi-fw pi-calendar"
+                }
+            ]
+        };
+    },
+    components: {
+        // HelloWorld
+        Menubar
+    }
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/bootstrap4-dark-blue/theme.css"
+import "primeicons/primeicons.css"
+import "primevue/resources/primevue.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+let app
 
-createApp(App).use(router).use(store).use(store).mount('#app')
+
+app = createApp(App)
+app.use(PrimeVue)
+app.use(router)
+app.use(store)
+app.use(store)
+app.mount('#app')
